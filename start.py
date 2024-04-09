5b37efe09de0369c7e332449753906b93e3e38a0
NicolasWltr <nicolas-walter1@gmx.de>
2024-04-09 18:14:17 +0200

    apiWP

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 from main import socket, app
 
 if __name__ == "__main__":
-    socket.run(app, host='0.0.0.0', port=25565, allow_unsafe_werkzeug=True)
+    socket.run(app, host='0.0.0.0', allow_unsafe_werkzeug=True)
