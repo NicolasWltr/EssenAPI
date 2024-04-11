3449059b3eae3d1b8171628504a3fe31216c9943
NicolasWltr <nicolas-walter1@gmx.de>
2024-04-11 21:54:14 +0200

    Creds

--- main.py
+++ main.py
@@ -376,10 +376,10 @@ def shutdownstop():
     return response.text
 
 
-import gameCalls
-
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
     # app.run(host='0.0.0.0', threaded=True)
+
+import gameCalls
 
 
