3cba4b21edf12d40f96815067a374d8a1588d253
NicolasWltr <nicolas-walter1@gmx.de>
2024-04-13 12:01:12 +0200

    sid

--- gameCalls.py
+++ gameCalls.py
@@ -2,14 +2,29 @@ from flask import request
 
 
 def init(socketio):
+
+    gamePins = []
+    clients = []
+
     @socketio.on('connect')
     def connect():
         sid = request.sid
+        clients.append(sid)
         print('Client connected', sid)
 
     @socketio.on('disconnect')
     def disconnect():
+        sid = request.sid
+        clients.remove(sid)
         print('Client disconnected')
+
+    @socketio.on('newGamePin')
+    def newGamePin():
+        sid = request.sid
+        pin = '123456'
+        gamePins.append(sid)
+        socketio.emit('gamePin', pin, room=sid)
+
 
     @socketio.on('hello')
     def hello(message):
