cc0cfd3d4037a8acd5e070f4f784cecea928b83a
NicolasWltr <nicolas-walter1@gmx.de>
2024-04-15 19:26:25 +0200

    con to Chat

--- gameCalls.py
+++ gameCalls.py
@@ -4,95 +4,103 @@ import random
 
 def init(socketio):
     gamePins = {}
-    clients = []
+    clientSid = {}
+
+    nextClient = [1000]
 
     @socketio.on('connect')
     def connect():
-        sid = request.sid
-        clients.append(sid)
-        print('Client connected', sid)
+        print('Client connected')
 
-    @socketio.on('disconnect')
-    def disconnect():
+    @socketio.on('getClient')
+    def getClient():
         sid = request.sid
-        clients.remove(sid)
+        socketio.emit('getClient', nextClient[0], room=sid)
+        updateClient(nextClient[0], sid)
+        nextClient[0] += 1
+
+    @socketio.on('disconnectClient')
+    def disconnect(client):
+        clientSid.pop(client)
         removeUnusedGamePin()
         print(gamePins)
         print('Client disconnected')
 
     @socketio.on('newGamePin')
-    def newGamePin():
-        sid = request.sid
+    def newGamePin(client):
+        updateClient(client, request.sid)
         pin = genPin()
         gamePins[pin] = []
-        gamePins[pin].append(sid)
+        gamePins[pin].append(client)
         print(gamePins)
-        socketio.emit('gamePin', pin, room=sid)
+        socketio.emit('gamePin', pin, room=getSid(client))
 
     @socketio.on('connectToPin')
-    def connectToPin(pin):
+    def connectToPin(pin, client):
+        updateClient(client, request.sid)
         removeUnusedGamePin()
         removeUnusedSidFromGame()
-        sid = request.sid
         if pin not in gamePins:
             print('Pin not found', pin)
-            socketio.emit('ConnectedToGame', "Game Pin not found", room=sid)
+            socketio.emit('ConnectedToGame', "Game Pin not found", room=getSid(client))
         if len(gamePins[pin]) >= 2:
             print('Game full', pin)
-            socketio.emit('ConnectedToGame', "Game full", room=sid)
+            socketio.emit('ConnectedToGame', "Game full", room=getSid(client))
             return
 
         print('Connecting to game pin', pin)
-        gamePins[pin].append(sid)
+        gamePins[pin].append(client)
         mesAllMem(pin)
 
     @socketio.on('chatToGame')
-    def chatToGame(mes):
-        sid = request.sid
+    def chatToGame(mes, client):
+        updateClient(client, request.sid)
 
-        mes = sid + "\n-> " + mes
+        mes = client + "\n-> " + mes
 
-        gp = gpForSid(sid)
+        gp = gpForSid(client)
 
         if gp == "":
             return
 
         for user in gamePins[gp]:
-            socketio.emit('chat', mes, room=user)
+            socketio.emit('chat', mes, room=getSid(user))
 
     @socketio.on('GetGameState')
-    def getGameState():
-        sid = request.sid
+    def getGameState(client):
+        updateClient(client, request.sid)
 
-        gp = gpForSid(sid)
+        gp = gpForSid(client)
 
         for user in gamePins[gp]:
-            if user == sid:
-                return
+            if user == client:
+                break
 
-            socketio.emit('GameStateReq', sid, room=user)
+            socketio.emit('GameStateReq', client, room=getSid(user))
+            return
 
     @socketio.on('RespWithGameState')
-    def RespWithGameState(state, sid):
-        print(sid)
+    def RespWithGameState(state, client):
+        updateClient(client, request.sid)
+        print(client)
         print(state)
-        socketio.emit('GameStateResp', state, room=sid)
+        socketio.emit('GameStateResp', state, room=getSid(client))
 
     @socketio.on('UpdateCons')
-    def UpdateCons(state, gPin):
-        sid = request.sid
+    def UpdateCons(state, gPin, client):
+        updateClient(client, request.sid)
         if gPin not in gamePins:
             print('Pin not found', gPin)
 
         for user in gamePins[gPin]:
-            if user == sid:
+            if user == client:
                 pass
             print("update ", user)
-            socketio.emit('getUpdatedFromPIN', state, room=user)
+            socketio.emit('getUpdatedFromPIN', state, room=getSid(user))
 
     def mesAllMem(pin):
         for user in gamePins[pin]:
-            socketio.emit('ConnectedToGame', "Success", room=user)
+            socketio.emit('ConnectedToGame', "Success", room=getSid(user))
 
     def genPin():
         pin = random.randint(100000, 999999)
@@ -110,26 +118,36 @@ def init(socketio):
         for pin in pinsToRem:
             del gamePins[pin]
 
-        removeUnusedSidFromGame()
-
     def removeUnusedSidFromGame():
-        sidToRemove = {}
+        clientToRemove = {}
         for game in gamePins:
             for user in gamePins[game]:
-                if user not in clients:
-                    if not sidToRemove.__contains__(user):
-                        sidToRemove[user] = []
-                    sidToRemove[user].append(game)
+                if user not in clientSid:
+                    if not clientToRemove.__contains__(user):
+                        clientToRemove[user] = []
+                    clientToRemove[user].append(game)
 
-        for user in sidToRemove:
-            for game in sidToRemove[user]:
+        for user in clientToRemove:
+            for game in clientToRemove[user]:
                 gamePins[game].remove(user)
 
-    def gpForSid(sid):
+    def gpForSid(client):
         gp = ""
         for game in gamePins:
             for user in gamePins[game]:
-                if user == sid:
+                if user == client:
                     gp = game
 
         return gp
+
+    def updateClient(client, sid):
+        if client in clientSid:
+            clientSid.update({client: sid})
+        else:
+            clientSid[client] = sid
+
+    def getSid(client):
+        if client in clientSid:
+            return clientSid[client]
+
+        return "Undefined"
