d4393872c6c73b3629a42808eb5aaac785263e4a
NicolasWltr <nicolas-walter1@gmx.de>
2024-04-27 16:02:58 +0200

    con to Chat

--- realTimeCom.py
+++ realTimeCom.py
@@ -1,8 +1,9 @@
 from flask import request
+import random
 
 
 def init(socketio):
     @socketio.on('RTCom')
     def handle_RTCom(client):
-        socketio.emit('RTCom', room=request.sid)
+        socketio.emit('RTCom', random.randint(0, 1000), room=request.sid)
 
